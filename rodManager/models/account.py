77296c42e9c077b444a2ac3b27f8868aaade88a5
Kubisopplay <38842052+Kubisopplay@users.noreply.github.com>
2023-10-20 00:08:17 +0200

    swagger prep + account model

--- rodManager/models/account.py
+++ rodManager/models/account.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+
+class Account(models.Model):
+    email = models.EmailField(primary_key=True)
+    password = models.CharField(max_length=128)
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    permission = models.IntegerField()
