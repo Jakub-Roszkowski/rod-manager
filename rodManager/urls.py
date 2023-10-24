e42bfb0b633cb871b0d22c305fd3d1b1accf200f
Tomasz Płóciennik SQ3ESP <83788346+sq3esp@users.noreply.github.com>
2023-10-24 18:29:14 +0200

    Add image uploading

--- rodManager/urls.py
+++ rodManager/urls.py
@@ -3,6 +3,8 @@ from rest_framework_swagger.views import get_swagger_view
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -21,6 +23,7 @@ schema_view = get_schema_view(
 
 from .views.register import *
 from .views.logout import *
+from .views.image import *
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -39,4 +42,8 @@ urlpatterns = [
     path("api/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/register/", RegistrationView.as_view(), name="register"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/image/", ImageView.as_view(), name="image"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
