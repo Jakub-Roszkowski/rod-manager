85e22a62b885eb92cc09bba9ac74f9a49fc3f478
Tomasz Płóciennik SQ3ESP <83788346+sq3esp@users.noreply.github.com>
2023-10-24 18:29:22 +0200

    Modify models

--- rodManager/dir_models/announcement.py
+++ rodManager/dir_models/announcement.py
@@ -2,7 +2,9 @@ from django.db import models
 
 
 class Announcement(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
     date = models.DateTimeField()
-    id = models.AutoField(primary_key=True)
+    tags = models.ManyToManyField("Tag")
+    pictures = models.ManyToManyField("Picture")
