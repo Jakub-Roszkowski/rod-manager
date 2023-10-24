d012305fef46e6401d355c2ec2b74f6f8cfa0502
Tomasz Płóciennik SQ3ESP <83788346+sq3esp@users.noreply.github.com>
2023-10-24 16:45:19 +0200

    Add phone number field

--- rodManager/dir_models/account.py
+++ rodManager/dir_models/account.py
@@ -5,11 +5,14 @@ from rodManager.users.manager import CustomUserManager
 
 from django.contrib.auth.models import AbstractUser
 
+
 class Account(AbstractUser):
     username = None
     REQUIRED_FIELDS = ["first_name", "last_name"]
     email = models.EmailField(unique=True)
     USERNAME_FIELD = "email"
+    phone = models.CharField(max_length=20, blank=True, null=True)
     objects = CustomUserManager()
+
     def __str__(self):
         return self.email
