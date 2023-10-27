97cb471354af361f77eb309a2bf710e2d96b0055
Tomasz Płóciennik SQ3ESP <83788346+sq3esp@users.noreply.github.com>
2023-10-27 11:45:51 +0200

    Enable tokens

--- management/settings.py
+++ management/settings.py
@@ -131,6 +131,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
 
 
