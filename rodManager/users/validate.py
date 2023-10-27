74e789866cc296e43b77098f1bc772fe56d06b69
Tomasz Płóciennik SQ3ESP <83788346+sq3esp@users.noreply.github.com>
2023-10-27 14:24:44 +0200

    Allow users to use functions without login

--- rodManager/users/validate.py
+++ rodManager/users/validate.py
@@ -13,23 +13,16 @@ def permission_required(permissionsList=None):
                 if request.request.user.groups.filter(name="ADMIN").exists():
                     return view_func(request, *args, **kwargs)
                 if isinstance(permissionsList, str):
-                    if not request.request.user.has_perm(permissionsList):
-                        return Response(
-                            {
-                                "error": "You do not have permission to perform this action."
-                            },
-                            status=status.HTTP_403_FORBIDDEN,
-                        )
+                    if request.request.user.has_perm(permissionsList):
+                        return view_func(request, *args, **kwargs)
                 else:
                     for permission in permissionsList:
-                        if not request.request.user.has_perm(permission):
-                            return Response(
-                                {
-                                    "error": "You do not have permission to perform this action."
-                                },
-                                status=status.HTTP_403_FORBIDDEN,
-                            )
-                return view_func(request, *args, **kwargs)
+                        if request.request.user.has_perm(permission):
+                            return view_func(request, *args, **kwargs)
+                return Response(
+                    {"error": "You do not have permission to perform this action."},
+                    status=status.HTTP_403_FORBIDDEN,
+                )
             else:
                 return Response(
                     {"error": "You must be logged in to perform this action."},
