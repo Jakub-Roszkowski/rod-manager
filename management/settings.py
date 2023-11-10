a447f63e67d00a0a3ba67eb6517aa521957cb7e3
Jakub Oleszczuk <38842052+Kubisopplay@users.noreply.github.com>
2023-11-10 08:24:03 +0100

    Would be cool if it worked outside of docker

--- management/settings.py
+++ management/settings.py
@@ -26,7 +26,10 @@ DEBUG = bool(os.environ.get("DEBUG", default=0))
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default="[]").split(" ")
+if os.environ.get("DJANGO_ALLOWED_HOSTS", default="[]").split(" ") == []:
+    DJANGO_ALLOWED_HOSTS = ["*"] 
+else:
+    DJANGO_ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default="[]").split(" ")
 
 
 # Application definition
