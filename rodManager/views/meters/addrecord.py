d007e47cecef00146677ee8d62292d8b87eb4894
Jakub Oleszczuk <38842052+Kubisopplay@users.noreply.github.com>
2023-11-24 11:26:09 +0100

    Work in Progress

--- rodManager/views/meters/addrecord.py
+++ rodManager/views/meters/addrecord.py
@@ -1,19 +1,49 @@
 
-from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Meter, Record
-from .serializers import RecordSerializer
+from rodManager.dir_models.meter import Meter
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
-class AddRecordView(APIView):
-    def post(self, request, meter_id):
-        try:
-            meter = Meter.objects.get(id=meter_id)
-        except Meter.DoesNotExist:
-            return Response({"error": "Meter not found"}, status=status.HTTP_404_NOT_FOUND)
-        
-        serializer = RecordSerializer(data=request.data)
-        if serializer.is_valid():
-            record = serializer.save(meter=meter)
-            return Response(RecordSerializer(record).data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@swagger_auto_schema(
+    request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            "id": openapi.Schema(type=openapi.TYPE_INTEGER),
+            "type": openapi.Schema(type=openapi.TYPE_STRING),
+            "adress": openapi.Schema(type=openapi.TYPE_STRING),
+            "garden": openapi.Schema(type=openapi.TYPE_INTEGER),
+        },
+        required=["id", "type"],
+    ),
+    responses={
+        201: openapi.Response(
+            description="Record added.",
+        ),
+        400: openapi.Response(
+            description="Bad request.",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={"error": openapi.Schema(type=openapi.TYPE_STRING)},
+            ),
+        ),
+    },
+)
+@api_view(['POST'])
+def add_record(request):
+    if "rodManager.manageRecords" in request.user.get_all_permissions():
+        if not request.data["id"] or not request.data["type"]:
+            return Response({"error": "ID and type are required"}, status=status.HTTP_400_BAD_REQUEST)
+        if not request.data["adress"] and not request.data["garden"]:
+            return Response({"error": "Adress or garden are required"}, status=status.HTTP_400_BAD_REQUEST)
+        if Meter.objects.filter(id=request.data["id"]).exists():
+            return Response({"error": "Meter already exists"}, status=status.HTTP_400_BAD_REQUEST)
+        newmeter = Meter.objects.create(
+            id=request.data["id"],
+            type=request.data["type"],
+            adress=request.data["adress"],
+            garden=request.data["garden"],
+        )
+        newmeter.save()
+        return Response({"success": "Meter added successfully"}, status=status.HTTP_201_CREATED)
