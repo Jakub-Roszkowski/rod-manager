d52d90c71273f6675efc0e65fb2f088bd41cffee
Tomasz Płóciennik SQ3ESP <83788346+sq3esp@users.noreply.github.com>
2023-11-24 10:02:15 +0100

    Update urls.py

--- rodManager/views/announcements/urls.py
+++ rodManager/views/announcements/urls.py
@@ -8,7 +8,7 @@ import rodManager.views.announcements.events as events
 import rodManager.views.announcements.tags as tags
 
 urlpatterns = [
-    path("announcement/", announcement.AnnouncementView.as_view(), name="image"),
+    path("", announcement.AnnouncementView.as_view(), name="image"),
     path("tag/", tags.TagView.as_view(), name="tag"),
     path("event/", events.EventView.as_view(), name="event"),
     path(
