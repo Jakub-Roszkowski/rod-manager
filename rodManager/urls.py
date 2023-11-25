bca9ebd9382b3d37595ce8ad9e6213a254b0f3f4
Tomasz Płóciennik SQ3ESP <83788346+sq3esp@users.noreply.github.com>
2023-11-25 23:19:09 +0100

    Simple enable payments handling

--- rodManager/urls.py
+++ rodManager/urls.py
@@ -55,6 +55,7 @@ urlpatterns = [
     path("api/garden-offers/", include("rodManager.views.gardenoffers.urls")),
     path("api/announcements/", include("rodManager.views.announcements.urls")),
     path("api/accounts/", include("rodManager.views.accounts.urls")),
+    path("api/payments/", include("rodManager.views.payments.urls")),
 ]
 
 if settings.DEBUG:
