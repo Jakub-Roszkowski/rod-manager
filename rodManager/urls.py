6e04a8b5994c85c87ca13a06056cee2afa79f42b
Tomasz Płóciennik SQ3ESP <tomek@plociennik.info>
2023-11-26 14:11:14 +0100

    Add whoami endpoint

--- rodManager/urls.py
+++ rodManager/urls.py
@@ -55,6 +55,7 @@ urlpatterns = [
     path("api/garden-offers/", include("rodManager.views.gardenoffers.urls")),
     path("api/announcements/", include("rodManager.views.announcements.urls")),
     path("api/accounts/", include("rodManager.views.accounts.urls")),
+    path("api/whoami/", include("rodManager.views.whoami.urls")),
 ]
 
 if settings.DEBUG:
---
+++ rodManager/views/whoami.py
@@ -0,0 +1,44 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
+from rest_framework.views import APIView
+
+
+class WhoamiView(APIView):
+
+    @swagger_auto_schema(
+        responses={
+            200: openapi.Response(
+                description="User information.",
+                schema=openapi.Schema(
+                    type=openapi.TYPE_ARRAY,
+                    items=openapi.Items(
+                        type=openapi.TYPE_OBJECT,
+                        properties={
+                            "id": openapi.Schema(type=openapi.TYPE_INTEGER),
+                            "username": openapi.Schema(type=openapi.TYPE_STRING),
+                            "email": openapi.Schema(type=openapi.TYPE_STRING),
+                            "first_name": openapi.Schema(type=openapi.TYPE_STRING),
+                            "last_name": openapi.Schema(type=openapi.TYPE_STRING),  
+                        },
+                    ),
+                ),
+            ),
+        },
+    )
+    def get(self, request):
+        if not request.user.is_authenticated:
+            return Response(
+                {"error": "You are not logged in."},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+        return Response(
+            {
+                "id": request.user.id,
+                "username": request.user.username,
+                "email": request.user.email,
+                "first_name": request.user.first_name,
+                "last_name": request.user.last_name,
+            }
+        )
+        
