a2f4bf13694af807eb2a11d36c766bb8cd617a6f
Jakub Oleszczuk <38842052+Kubisopplay@users.noreply.github.com>
2023-11-27 08:24:33 +0100

    Fixed autonumber in id fields

--- rodManager/dir_models/meter.py
+++ rodManager/dir_models/meter.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Meter(models.Model):
-    id = models.CharField(max_length=200, primary_key=True)
+    id = models.AutoField(primary_key=True)
     type = models.CharField(max_length=200)
     adress = models.CharField(max_length=200)
     garden = models.ForeignKey()
