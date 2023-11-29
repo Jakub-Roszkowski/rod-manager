97d57fd2f3cfbd02666870886a075f9e52c716d7
Tomasz Płóciennik SQ3ESP <83788346+sq3esp@users.noreply.github.com>
2023-11-29 18:53:44 +0100

    Add document handling

---
+++ rodManager/dir_models/document.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Document(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey(
+        "self", null=True, blank=True, related_name="children", on_delete=models.CASCADE
+    )
+    file = models.FileField(upload_to="documents/", null=True, blank=True)
+
+    def to_dict(self):
+        result = {"id": self.id, "name": self.name}
+        if self.file:
+            result["file_url"] = self.file.url
+        children = self.children.all()
+        if children:
+            result["items"] = [child.to_dict() for child in children]
+        return result
+
+    @classmethod
+    def create_document(cls, name, parent=None, file=None):
+        new_document = cls(name=name, parent=parent, file=file)
+        new_document.save()
+        return new_document
+
+    def delete(self, *args, **kwargs):
+        if self.file:
+            self.file.delete()
+        for child in self.children.all():
+            child.delete()
+        super().delete(*args, **kwargs)
---
+++ rodManager/views/documents/documentbyid.py
@@ -0,0 +1,70 @@
+from django.contrib.auth.models import Group
+from drf_spectacular.utils import (
+    OpenApiParameter,
+    OpenApiResponse,
+    OpenApiTypes,
+    extend_schema,
+    inline_serializer,
+)
+from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from rodManager.dir_models.document import Document
+from rodManager.users.validate import permission_required
+
+
+class UpdateDocumentSerializer(serializers.Serializer):
+    name = serializers.CharField(allow_null=False, required=False)
+    parent = serializers.PrimaryKeyRelatedField(
+        queryset=Document.objects.filter(file__isnull=True),
+        allow_null=True,
+        required=False,
+    )
+    file = serializers.FileField(allow_null=True, required=False)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.parent = validated_data.get("parent", instance.parent)
+        if validated_data.get("file", None):
+            if instance.file:
+                instance.file.delete()
+        instance.file = validated_data.get("file", instance.file)
+        instance.save()
+        return instance
+
+
+class DocumentByIdSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    parent = serializers.PrimaryKeyRelatedField(
+        queryset=Document.objects.all(), allow_null=True
+    )
+    file = serializers.FileField(allow_null=True, required=False)
+
+
+class DocumentByIdView(APIView):
+    @extend_schema(
+        summary="Get document",
+        description="Get document by id.",
+        request=UpdateDocumentSerializer,
+        responses={200: DocumentByIdSerializer},
+    )
+    def put(self, request, document_id):
+        document = Document.objects.get(pk=document_id)
+        serializer = UpdateDocumentSerializer(document, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        response = DocumentByIdSerializer(serializer.save()).data
+        return Response(response)
+
+    @extend_schema(
+        summary="Delete document",
+        description="Delete document by id.",
+        responses={204: None},
+    )
+    def delete(self, request, document_id):
+        document = Document.objects.get(pk=document_id)
+        if document.file:
+            document.file.delete()
+        document.delete()
+        return Response(status=204)
