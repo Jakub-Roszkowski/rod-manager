668c1c32a40a732d35c64a89444eafc55513addd
Jakub Oleszczuk <38842052+Kubisopplay@users.noreply.github.com>
2023-12-08 11:03:43 +0100

    RODInfo fixed

--- rodManager/urls.py
+++ rodManager/urls.py
@@ -61,7 +61,7 @@ urlpatterns = [
     path("api/accounts/", include("rodManager.views.accounts.urls")),
     path("api/pools/", include("rodManager.views.pools.urls")),
     path("api/my-garden/", include("rodManager.views.myGardenPlot.urls")),
-    path("api/garden-info/", include("rodManager.views.gardenInfo.urls")),
+    path("api/garden-info/", include("rodManager.views.RODInfo.urls")),
     path("api/technical-problem/", include("rodManager.views.technicalProblem.urls")),
     path("api/who-am-i/", WhoamiView.as_view(), name="whoami"),
     path("api/payments/", include("rodManager.views.payments.urls")),
